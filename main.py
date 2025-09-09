d4ee704b8fbe77c64ef9f819b8436f60362c7dd7
Hant <danielnida@gmail.com>
2025-09-09 00:30:05 -0400

    updating so i can work on my laptop later... :3

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-# attempt to make a basic idle game with tkinter
-import tkinter
+# attempt to make a basic idle game with pygame
+import pygame as pg
+import time
 
-# Tk(screenName=None, baseName=None, className='Tk', useTk=1)
-win = tkinter.Tk()
+###
+#GLOBAL VARIABLES
+###
+# program window dimensions
+winY = 400
+winX = 800
+# defining colors
+black = pg.Color(41, 41, 41)
+white = pg.Color(222, 222, 222)
+red = pg.Color(237, 68, 171)
+green = pg.Color(125, 230, 30)
+blue = pg.Color(116, 202, 255)
 
-win.mainloop()
+#initialize pygame
+pg.init()
+
+# set up window
+screen = pg.display.set_mode((winX, winY))
+#program caption name
+pg.display.set_caption("Hewwo World :3")
+
+##
+#FUNCTIONS
+##
+
+
+
+
+##
+#MAIN LOOP
+##
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+
+pg.quit()
